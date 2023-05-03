30cd29b5ead4806de0d6e05745442af43adff0c5
unknown <ssanym211@gmail.com>
2023-05-03 21:05:13 +0300

    second commit

---
+++ exem.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class exem {
+   
+    public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int n=7;
+    switch(n){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        System.out.println("1");
+        break;
+        default:
+        System.out.println("Такого числа нет");
+        break;
+    }
+    for(int i=0; i<10; i++){
+     if(i-2==2){
+       break; 
+     }
+     System.out.println(i);
+     }
+    
+    }
+   
+    }
+
